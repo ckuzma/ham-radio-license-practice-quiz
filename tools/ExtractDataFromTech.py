b729be588b9a822f4aea5c3d7be184230f595935
ckuzma <5973601+ckuzma@users.noreply.github.com>
2021-09-10 17:50:48 -0700

    Interim save, got a bug to fix

--- tools/ExtractDataFromTech.py
+++ tools/ExtractDataFromTech.py
@@ -15,23 +15,29 @@ while x < len(questions):
     questions[x].pop(len(questions[x]) - 1)
 
     # Parse out the question ID, answer, and reference number
-    id_answer_ref = questions[x][1].split(' ')
-    answer = id_answer_ref[1][1:2]
-    ref = id_answer_ref[2][1:len(id_answer_ref[2])-1]
+    id_answer_ref = None
+    try:
+        id_answer_ref = questions[x][1].split(' ')
+        answer = id_answer_ref[1][1:2]
+        ref = id_answer_ref[2][1:len(id_answer_ref[2])-1]
 
-    # Parse out the question
-    single_question = questions[x][2]
+        # Parse out the question
+        single_question = questions[x][2]
 
-    # Parse out the possible answers
-    answers = questions[x][3:]
+        # Parse out the possible answers
+        answers = questions[x][3:]
+        
+        # Add to question pool
+        question_pool[id] = {
+            'correct_answer': answer,
+            'possible_answers': answers,
+            'question': single_question,
+            'reference_num': ref
+        }
+    except:
+        print("EXCEPTION")
+        # print(questions[x])
     
-    # Add to question pool
-    question_pool[id] = {
-        'correct_answer': answer,
-        'possible_answers': answers,
-        'question': single_question,
-        'reference_num': ref
-    }
     x+=1
 
 # print(question_pool)
